2ee6750375bc364cd5fa8b68cfa7b6b31e51d018
André Kirst <github@andrekirst.de>
2018-07-04 20:29:45 +0200

    #22 - Tests und Implementierung

---
+++ WordCount/Implementations/ArgumentsHandling/StopwordListParameterParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using WordCount.Abstractions.Console;
+using WordCount.Extensions;
+using WordCount.Interfaces.ArgumentsHandling;
+using WordCount.Models;
+
+namespace WordCount.Implementations.ArgumentsHandling
+{
+    public class StopwordListParameterParser : IStopwordListParameterParser
+    {
+        private readonly IEnvironment _environment;
+
+        public StopwordListParameterParser(IEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public StopwordListParameter ParseStopwordListParameter()
+        {
+            string[] args = _environment.GetCommandLineArgs();
+
+            string dictionaryParameter = args?.FirstOfMatchingRegex(pattern: @"-stopwordlist=[a-zA-z.]{1,}");
+            string[] parameterSplitByEqualSign = dictionaryParameter?.Split('=');
+
+            return new StopwordListParameter
+            {
+                IsPresent = dictionaryParameter?.IsFilled() ?? false,
+                FileName = parameterSplitByEqualSign?.LastOrDefault()
+            };
+        }
+    }
+}
---
+++ WordCount/Models/StopwordListParameter.cs
@@ -0,0 +1,9 @@
+﻿namespace WordCount.Models
+{
+    public class StopwordListParameter
+    {
+        public bool IsPresent { get; set; }
+
+        public string FileName { get; set; }
+    }
+}
