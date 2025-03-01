0b2d37c92c6fca2553d945d6f6836ca7a4819060
André Kirst <github@andrekirst.de>
2025-03-01 22:46:20 +0100

    register services

--- src/cs/WordCount/Program.cs
+++ src/cs/WordCount/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO.Abstractions;
+using System.Net.Http;
+using System.Resources;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
@@ -25,6 +28,23 @@ public sealed class WordCountCommand : AsyncCommand<WordCountCommand.Settings>
 
         builder.Services.AddSingleton(_ => Options.Create(settings));
         builder.Services.AddSingleton<IInteractor, Interactor>();
+        builder.Services.AddSingleton<ITextInput, TextInput>();
+        builder.Services.AddSingleton<ITextFileLoader, TextFileLoader>();
+        builder.Services.AddSingleton<IFileSystem, FileSystem>();
+        builder.Services.AddSingleton<IDisplayOutput, ConsoleDisplayOutput>();
+        builder.Services.AddSingleton<ILanguageResource, LanguageResource>();
+        builder.Services.AddSingleton<ILanguageDecision, LanguageDecision>();
+        builder.Services.AddSingleton<ITextUrlFileLoader, TextUrlFileLoader>();
+        builder.Services.AddSingleton(_ => new HttpClient());
+        builder.Services.AddSingleton<IWordCountAnalyzer, WordCountAnalyzer>();
+        builder.Services.AddSingleton<ITextSplit, TextSplit>();
+        builder.Services.AddSingleton<IStopwordRemover, StopwordRemover>();
+        builder.Services.AddSingleton<IStopwordLoader, StopwordLoader>();
+        builder.Services.AddSingleton<IWordCountAnalyzerOutput, WordCountAnalyzerOutput>();
+        builder.Services.AddSingleton<IStatisticsOutput, StatisticsOutput>();
+        builder.Services.AddSingleton<IIndexOutput, IndexOutput>();
+        builder.Services.AddSingleton<IDictionaryFileLoader, DictionaryFileLoader>();
+        builder.Services.AddSingleton(_ => new ResourceManager("WordCount.Resources.Resource", typeof(Program).Assembly));
 
         var host = builder.Build();
 
